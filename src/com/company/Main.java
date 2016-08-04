68777bdb2b31986849a339f4474a1474355314fa
Maxim <astratenko.m@gmail.com>
2016-08-04 23:36:07 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import org.jetbrains.annotations.Contract;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Please,");
+        Scanner sc = new Scanner(System.in);
+        double firstCount = sc.nextDouble();
+
+        System.out.println("What do you need to do?");
+        String symbol = sc.next();
+
+        double secondNumber = sc.nextDouble();
+
+
+        switch (symbol) {
+            case "+":
+                System.out.println(sum(firstCount, secondNumber));
+                break;
+            case "-":
+                System.out.println(subtract(firstCount, secondNumber));
+                break;
+            case "/":
+                System.out.println(divide(firstCount, secondNumber));
+                break;
+            case "*":
+                System.out.println(multiply(firstCount, secondNumber));
+                break;
+        }
+    }
+
+    @Contract(pure = true)
+    private static double sum(double newFirstNumber, double newSecondNumber) {
+        double sumResult = newFirstNumber + newSecondNumber;
+        return sumResult;
+    }
+
+    @Contract(pure = true)
+    private static double subtract(double newFirstNumber, double newSecondNumber) {
+        double subtractResult = newFirstNumber - newSecondNumber;
+        return subtractResult;
+    }
+
+    @Contract(pure = true)
+    private static double multiply(double newFirstNumber, double newSecondNumber) {
+        double multiplyResult = newFirstNumber * newSecondNumber;
+        return multiplyResult;
+    }
+
+    @Contract(pure = true)
+    private static double divide(double newFirstNumber, double newSecondNumber) {
+        double divideResult = newFirstNumber / newSecondNumber;
+        return divideResult;
+    }
+
+}
